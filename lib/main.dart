6428649861275a3a9d22631a2129ffce9bddc6d9
elifelipe <elifelipe2012@gmail.com>
2023-04-17 16:36:04 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:elipsy_gift/ui/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(highlightColor: Colors.white),
+  ));
+}
